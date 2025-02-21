16c058ad2da95dde60228e038ab2d944b970c934
Kundan Kumar Gupta <kundankrgupta@outlook.com>
2025-02-21 17:35:23 +0530

    server update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Components/Navbar"
 import AllRoutes from "./Routes/AllRoutes"
 
 // export const SERVER = "http://localhost:3000"
-export const SERVER = "https://server-gyandhan.onrender.com/"
+export const SERVER = "https://server-gyandhan.onrender.com"
 
 const App = () => {
 	return (
